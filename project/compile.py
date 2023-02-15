477dea177271d99a27771fbcdd12f3b21b36b661
mintydevdaz <mintydevdaz@proton.me>
2023-02-15 16:19:55 +1100

    2.6. replaces compile_data.py

---
+++ project/compile.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from pathlib import Path
+
+from opera import opera
+from spider import spider
+
+
+def main():
+    # Create folder on Desktop
+    try:
+        folder = "csv_files"
+        parent_dir = str(Path.home() / "Desktop/")
+        path = os.path.join(parent_dir, folder)
+        os.mkdir(path)
+        print(f"-> New folder created at: {path}")
+    except OSError as err:
+        sys.exit(f"-> Operation aborted: ***{err}***")
+
+    # Run spider.py & opera.py
+    print("-> Spinning up opera.py")
+    opera()
+    print("-> Spinning up spider.py")
+    spider()
+
+    # List directory files
+    print("-> New csv files succesfully created:")
+    for i, csv in enumerate(os.listdir(path), start=1):
+        print(f"{i}. {csv}")
+
+
+if __name__ == "__main__":
+    main()
