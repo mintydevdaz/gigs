5aaa86ef24a5c69ce56eb6dd5951ef2136b3aee3
mintydevdaz <mintydevdaz@proton.me>
2023-02-15 16:55:45 +1100

    2.10. created save funcs

--- project/clean.py
+++ project/clean.py
@@ -29,11 +29,27 @@ def main():
     new_col = df["Event_Date"].dt.strftime("%d-%b-%-y (%a)")
     df.insert(1, "Date", new_col)
 
-    # Drop Event_Date
-    df = df.drop("Event_Date", axis="columns")
+    # Save CSV files
+    gigs_table(fp, df)
+    pretty_table(fp, df, start_date)
 
-    # Save CSV
-    df.to_csv(f"{fp}/gigs.csv", index=False)
+
+def gigs_table(directory_path: str, df: pd.DataFrame):
+    f = "gigs.csv"
+    path = f"{directory_path}/{f}"
+    df = df.drop("Event_Date", axis="columns")
+    df.to_csv(path, index=False)
+    print(f"Saved to {path}")
+
+
+def pretty_table(directory_path: str, df: pd.DataFrame, start_date: str):
+    f = "pretty_table.csv"
+    path = f"{directory_path}/{f}"
+    end_date = str(date.today() + timedelta(days=30))
+    df = df.query("Event_Date > @start_date and Event_Date < @end_date")
+    df = df.drop("Event_Date", axis="columns")
+    df.to_csv(path, index=False)
+    print(f"Saved to {path}")
 
 
 if __name__ == "__main__":
