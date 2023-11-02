c758835ff1d8016b5985cb1ae2795f2ec9c55b99
mintydevdaz <mintydevdaz@proton.me>
2023-11-02 14:21:17 +1100

    modified fetching prices

--- gigs/scrapers/oztix.py
+++ gigs/scrapers/oztix.py
@@ -1,12 +1,10 @@
-from json import JSONDecodeError
 import logging
 import os
 import sys
 import unicodedata
-from datetime import datetime
 
 import httpx
-from pydantic import BaseModel, field_validator
+from pydantic import field_validator
 from selectolax.parser import HTMLParser
 
 from gigs.utils import (
@@ -38,24 +36,28 @@ def create_data_cache(response: httpx.Response, json_key: str) -> list[dict] | N
     return None
 
 
-def get_price(url: str) -> float:
-    response = get_request(url, headers)
-    if response is None:
-        logging.error(f"Error fetching price for {url}.")
-        return 0.0
-    try:
-        tree = HTMLParser(response.text)
-        return extract_ticket_price(tree)
-    except Exception as exc:
-        logging.error(f"Error fetching price for {url}: {exc}.")
-        return 0.0
+def get_prices(data: list[dict], price_tag: str):
+    result = []
+    with httpx.Client(headers=headers) as client:
+        for event in data:
+            url = event["url"]
+            try:
+                response = client.get(url)
+                tree = HTMLParser(response.text)
+                price = extract_ticket_price(tree, price_tag)
+                event["price"] = "price"
+                result.append(event)
+            except Exception as exc:
+                logging.error(f"Unable to extract price at URL '{url}': {exc}.")
+                result.append(event)
+    return result
 
 
-def extract_ticket_price(tree: HTMLParser) -> float:
-    nodes = tree.css("div.ticket-price.hide-mobile")
+def extract_ticket_price(html: HTMLParser, tag: str) -> float:
+    nodes = html.css(tag)
     prices = [
         float(
-            node.css_first("div.ticket-price.hide-mobile")
+            node.css_first(tag)
             .text()
             .strip()
             .replace("$", "")
@@ -66,14 +68,13 @@ def extract_ticket_price(tree: HTMLParser) -> float:
     return float(min(prices)) if prices else 0.0
 
 
-def fetch_data(event_data: list[dict]) -> list[dict]:
+def get_data(event_data: list[dict]) -> list[dict]:
     result = []
     for data in event_data:
         try:
             gig = OztixGig(
                 date=data["dateStart"],  # ISO8601
                 title=data["eventName"],
-                price=get_price(url),
                 venue=data["venue"]["name"],
                 suburb=data["venue"]["locality"],
                 state=data["venue"]["state"],
@@ -82,7 +83,7 @@ def fetch_data(event_data: list[dict]) -> list[dict]:
             )
             result.append(gig.model_dump())
         except Exception as exc:
-            logging.error(f"Unable to fetch data: {exc} ({data.get('EventUrl')})")
+            logging.error(f"Unable to fetch data: {exc} ({data.get('EventUrl', '-')})")
     return result
 
 
@@ -91,6 +92,9 @@ def fetch_data(event_data: list[dict]) -> list[dict]:
 def oztix():
     logging.warning(f"Running {os.path.basename(__file__)}")
 
+    # CSS Selector
+    PRICE_TAG = "div.ticket-price.hide-mobile"
+
     response = get_post_response(
         url="https://personalisationapi.oztix.com.au/api/recommendations",
         payload=payload,
@@ -98,14 +102,16 @@ def oztix():
     if response is None:
         sys.exit(1)
 
-    event_data = create_data_cache(response, json_key="catalog")
-    if event_data is None:
+    cache = create_data_cache(response, json_key="catalog")
+    if cache is None:
         logging.error("No JSON data found.")
         sys.exit(1)
 
-    result = fetch_data(event_data)
-    logging.warning(f"Found {len(result)} events.")
-    export_json(result, filepath=save_path("data", "oztix.json"))
+    initial_data = get_data(cache)
+    final_data = get_prices(initial_data, PRICE_TAG)
+    logging.warning(f"Found {len(final_data)} events.")
+
+    export_json(final_data, filepath=save_path("data", "oztix.json"))
 
 
 if __name__ == "__main__":
