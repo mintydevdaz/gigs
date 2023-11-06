4972aa171aeb54173ed19852f4c17ea6e75b280e
mintydevdaz <mintydevdaz@proton.me>
2023-11-07 08:43:45 +1100

    renamed headers

--- gigs/utils.py
+++ gigs/utils.py
@@ -65,7 +65,7 @@ def logger(filepath: str):
     return decorator
 
 
-headers = {
+custom_headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"  # noqa
 }
 
