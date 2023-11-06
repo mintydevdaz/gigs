bd537584a218ce34e40eb14444ad0b866c06d019
mintydevdaz <mintydevdaz@proton.me>
2023-11-07 09:25:20 +1100

    updated headers import

--- gigs/scrapers/moshtix_cache.py
+++ gigs/scrapers/moshtix_cache.py
@@ -6,10 +6,17 @@ import chompjs
 import httpx
 from selectolax.parser import HTMLParser, Node
 
-from gigs.utils import export_json, get_request, headers, logger, save_path, timer
+from gigs.utils import (
+    export_json,
+    get_request,
+    custom_headers,
+    logger,
+    save_path,
+    timer,
+)
 
 
-def get_response_and_parse_html(url: str) -> HTMLParser | None:
+def get_response_and_parse_html(url: str, headers: dict[str, str]) -> HTMLParser | None:
     response = get_request(url, headers)
     return None if response is None else HTMLParser(response.text)
 
@@ -25,19 +32,22 @@ def get_page_number(html: HTMLParser, tag: str) -> int | None:
         return None
 
 
-def end_page(base_url: str, tag: str, start_page: int = 1) -> int | None:
+def end_page(
+    base_url: str, headers: dict[str, str], tag: str, start_page: int = 1
+) -> int | None:
     url = f"{base_url}{start_page}"
-    html = get_response_and_parse_html(url)
+    html = get_response_and_parse_html(url, headers)
     return None if html is None else get_page_number(html, tag)
 
 
 def get_event_nodes(
-    base_url: str, json_tag: str, start_page: int = 1, end_page: int = 2
+    base_url: str, headers: dict[str, str], json_tag: str, start_page: int = 1, end_page: int = 2
 ) -> list[Node]:
     event_nodes = []
-    with httpx.Client() as client:
+    with httpx.Client(headers=headers) as client:
         for page in range(start_page, end_page):
             url = f"{base_url}{page}"
+            logging.warning(url)
             try:
                 response = client.get(url)
                 tree = HTMLParser(response.text)
@@ -64,18 +74,19 @@ def moshtix_cache():
     PAGE_TAG = "section.moduleseparator"
     JSON_TAG = "script[type='application/ld+json']"
     EVENT_TAG = "div.searchresult.clearfix"
-    BASE_URL = "https://www.moshtix.com.au/v2/search?query=&CategoryList=2&refine="
-    DESTINATION_FILE = "moshtix_cache.json"
+    base_url = "https://www.moshtix.com.au/v2/search?query=&CategoryList=2&refine="
+    destination_file = "moshtix_cache.json"
+    headers = custom_headers
 
-    page_num = end_page(BASE_URL, PAGE_TAG)
+    page_num = end_page(base_url, headers, PAGE_TAG)
     if page_num is None:
         logging.error("Error fetching last page number.")
         sys.exit(1)
 
-    event_nodes = get_event_nodes(BASE_URL, JSON_TAG, end_page=page_num)
+    event_nodes = get_event_nodes(base_url, headers, JSON_TAG, end_page=page_num)
     data = extract_event_data(event_nodes)
     logging.warning(f"Found {len(data)} events.")
-    export_json(data, filepath=save_path("cache", DESTINATION_FILE))
+    export_json(data, filepath=save_path("cache", destination_file))
 
 
 if __name__ == "__main__":
