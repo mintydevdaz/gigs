02881120127bb9f26e8622d632a57ee3ed7339e7
mintydevdaz <mintydevdaz@proton.me>
2024-01-08 23:39:10 +1100

    updated class objects

--- gigs/scrapers/ticketmaster.py
+++ gigs/scrapers/ticketmaster.py
@@ -4,36 +4,67 @@ import sys
 
 import httpx
 from dotenv import load_dotenv
+from gigs.utils import Gig, WebScraper, logger, save_path, timer
 
-from gigs.utils import Gig, export_json, logger, save_path, timer
+
+class TicketmasterScraper(WebScraper):
+    def __init__(self) -> None:
+        super().__init__()
+        load_dotenv()
+        self._api_key = str(os.getenv("TM_KEY"))
+        self.end_page = self._get_end_page(self._api_key)
+
+    def _get_end_page(self, api_key: str) -> int | None:
+        url = f"https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=AU&page=0&apikey={api_key}"  # noqa
+        try:
+            json = httpx.get(url).json()
+            return json["page"]["totalPages"]
+        except Exception as exc:
+            logging.error(f"Error fetching end page from JSON: {exc}.")
+            return None
+
+    def get_events(self, end_page: int, cache_file: str) -> list[dict] | None:
+        events = []
+        with httpx.Client() as client:
+            for page in range(end_page):
+                url = f"https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=AU&page={page}&apikey={self._api_key}"  # noqa
+                try:
+                    json = client.get(url).json()["_embedded"]["events"]
+                    events.extend(json)
+                except Exception as exc:
+                    logging.error(f"Error fetching JSON '{url}': {exc}.")
+        if not events:
+            return None
+        logging.warning(f"Found {events.__len__()} Ticketmaster events.")
+        self.export_json(events, filepath=save_path("data", cache_file))
+        return events
+
+    def get_data(self, events: list[dict], data_file: str) -> None:
+        result = []
+        for event in events:
+            url = event.get("url", "-")
+            venue, suburb, state = get_location_info(event)
+            try:
+                gig = TicketmasterGig(
+                    date=get_date(event),
+                    title=event.get("name", "-"),
+                    price=get_lowest_price(event),
+                    venue=venue,
+                    suburb=suburb,
+                    state=state,
+                    url=url,
+                    image=get_image(event),
+                )
+                result.append(gig.model_dump())
+            except Exception as exc:
+                logging.error(f"Error parsing data for '{url}': {exc}.")
+        logging.warning(f"Saved {result.__len__()} Ticketmaster events.")
+        self.export_json(result, filepath=save_path("data", data_file))
 
 
 class TicketmasterGig(Gig):
     genre: str = "Music"
     source: str = "Ticketmaster"
-
-
-def end_page(api_key: str) -> int | None:
-    url = f"https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=AU&page=0&apikey={api_key}"  # noqa
-    try:
-        json = httpx.get(url).json()
-        return json["page"]["totalPages"]
-    except Exception as exc:
-        logging.error(f"Error fetching end page from JSON: {exc}.")
-        return None
-
-
-def get_events(api_key: str, last_page: int) -> list[dict]:
-    events = []
-    with httpx.Client() as client:
-        for page in range(last_page):
-            url = f"https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=AU&page={page}&apikey={api_key}"  # noqa
-            try:
-                json = client.get(url).json()["_embedded"]["events"]
-                events.extend(json)
-            except Exception as exc:
-                logging.error(f"Error fetching JSON '{url}': {exc}.")
-    return events
 
 
 def get_date(event: dict) -> str:
@@ -73,28 +104,6 @@ def get_image(event: dict) -> str:
         return "-"
 
 
-def extract_data(events: list[dict]) -> list[dict]:
-    result = []
-    for event in events:
-        url = event.get("url", "-")
-        venue, suburb, state = get_location_info(event)
-        try:
-            gig = TicketmasterGig(
-                date=get_date(event),
-                title=event.get("name", "-"),
-                price=get_lowest_price(event),
-                venue=venue,
-                suburb=suburb,
-                state=state,
-                url=url,
-                image=get_image(event),
-            )
-            result.append(gig.model_dump())
-        except Exception as exc:
-            logging.error(f"Error parsing data for '{url}': {exc}.")
-    return result
-
-
 @timer
 @logger(filepath=save_path("data", "app.log"))
 def ticketmaster():
@@ -103,22 +112,16 @@ def ticketmaster():
     destination_cache_file = "ticketmaster_cache.json"
     destination_data_file = "ticketmaster.json"
 
-    load_dotenv()
-    api_key = str(os.getenv("TM_KEY"))
-
-    last_page = end_page(api_key)
-    if last_page is None:
+    scraper = TicketmasterScraper()
+    end_page = scraper.end_page
+    if end_page is None:
         sys.exit(1)
 
-    events = get_events(api_key, last_page)
-    if not len(events):
-        logging.error("No events found!")
+    events = scraper.get_events(end_page, destination_cache_file)
+    if events is None:
         sys.exit(1)
-    export_json(events, filepath=save_path("cache", destination_cache_file))
 
-    data = extract_data(events)
-    logging.warning(f"Found {len(data)} events.")
-    export_json(data, filepath=save_path("data", destination_data_file))
+    data = scraper.get_data(events, destination_data_file)
 
 
 if __name__ == "__main__":
