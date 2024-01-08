ee474c284704abdb5bfd6a7a453c4744e12bf2eb
mintydevdaz <mintydevdaz@proton.me>
2024-01-08 21:15:50 +1100

    updated with Class objects

--- gigs/scrapers/phoenix.py
+++ gigs/scrapers/phoenix.py
@@ -8,15 +8,7 @@ import httpx
 from pydantic import field_validator
 from selectolax.parser import HTMLParser
 
-from gigs.utils import (
-    Gig,
-    custom_headers,
-    export_json,
-    get_request,
-    logger,
-    save_path,
-    timer,
-)
+from gigs.utils import Gig, WebScraper, custom_headers, logger, save_path
 
 
 class PhoenixGig(Gig):
@@ -46,182 +38,98 @@ def format_date(date_str: str, fmt: str) -> str:
     return datetime.strptime(new_date, fmt).isoformat()
 
 
-def extract_homepage_text(response, base_url) -> str | None:
-    """
-    Extracts the URL of the season from the homepage text.
-
-    Args:
-        response: The HTTP response object containing the homepage text.
-        base_url: The base URL of the website.
-
-    Returns:
-        str | None: The constructed URL of the season, or None if unable to parse the
-        text.
-
-    Raises:
-        Exception: If an error occurs while parsing the text.
-
-    Example:
-        ```python
-        response = httpx.get("https://example.com/homepage")
-        base_url = "https://example.com"
-        url = extract_homepage_text(response, base_url)
-        if url:
-            print(f"Season URL: {url}")
-        else:
-            print("Failed to extract season URL.")
-        ```
-    """
-    try:
-        tree = HTMLParser(response.text)
-        season = next(
-            (
-                link.attributes["href"]
-                for link in tree.css("div.header-nav-folder-item > a")
-                if "season" in link.text().lower()
-            ),
-            "",
-        )
-        url = f"{base_url}{season}"
-        logging.warning(f"Constructed URL: {url}")
-        return url
-    except Exception as exc:
-        logging.error(f"Unable to parse text on homepage: {exc}")
-        return None
-
-
-def get_season_url(base_url: str, headers: dict[str, str]) -> str | None:
-    """
-    Fetches the URL for the current season from the given base URL.
-
-    The function sends an HTTP GET request to the specified `base_url` with the
-    provided `headers`. If the response is None, an error message is logged and None is
-    returned. Otherwise, it attempts to construct the URL for the current season by
-    calling the `extract_homepage_text` function with the response and base URL. If any
-    exceptions occur during the process, an error message is logged and None is
-    returned.
-
-    Args:
-        base_url (str): The base URL to fetch the current season URL from.
-
-    Returns:
-        str | None: The URL for the current season, or None if an error occurs.
-
-    Raises:
-        Exception: If any error occurs during the fetching process.
-    """
-    response = get_request(base_url, headers)
-    if response is None:
-        logging.error(f"Error fetching response for {base_url}.")
-        return None
-    season_url = extract_homepage_text(response, base_url)
-    return None if season_url is None else season_url
-
-
-def extract_links(html: HTMLParser) -> list[str] | None:
-    """
-    Extracts links from the given HTMLParser instance.
-
-    Args:
-        html (HTMLParser): The HTMLParser instance representing the parsed HTML.
-
-    Returns:
-        list[str] | None: A list of extracted links if successful, or None if no links
-        are found.
-
-    Example:
-        ```python
-        parser = HTMLParser(html)
-        extract_links(parser)
-        ```
-    """
-    links = html.css("a.sqs-block-image-link")
-    if not links:
-        return None
-
-    result = [
-        link.css_first("a").attrs.get("href")
-        for link in links
-        if link.css_first("a").attrs.get("href") is not None
-    ]
-    return result or None
-
-
-def get_event_urls(url: str, headers: dict[str, str]) -> list[str] | None:
-    """
-    Executes the process of fetching event URLs from the given URL.
-
-    Args:
-        url (str): The URL to fetch the response from.
-
-    Returns:
-        list[str] | None: A list of event URLs if successful, or None if there was an
-        error fetching the response.
-
-    Example:
-        ```python
-        get_event_urls("https://example.com")
-        ```
-    """
-    response = get_request(url, headers)
-    if not response:
-        logging.error(f"Error fetching response for {url}.")
-        return None
-
-    tree = HTMLParser(response.text)
-    return extract_links(tree)
-
-
 def get_image(html: HTMLParser, tag: str) -> str:
     meta_tag = html.css_first(tag)
     return meta_tag.attrs.get("content", "-")
 
 
-def get_data(
-    urls: list[str], headers: dict[str, str], title_tag: str, date_tag: str, image_tag: str
-) -> list[dict]:
-    result = []
-    with httpx.Client(headers=headers) as client:
-        for url in urls:
-            try:
-                response = client.get(url)
-                tree = HTMLParser(response.text)
-                gig = PhoenixGig(
-                    date=tree.css_first(date_tag).text(),
-                    title=tree.css_first(title_tag).text(),
-                    url=url,
-                    image=get_image(tree, image_tag),
-                )
-                result.append(gig.model_dump())
-            except Exception as exc:
-                logging.error(f"Unable to extract data from URL '{url}': {exc}.")
-    return result
+class PhoenixScraper(WebScraper):
+    def __init__(self) -> None:
+        super().__init__()
+        self.base_url = "https://phoenixcentralpark.com.au"
+        self.headers = custom_headers
+        self.current_season_url = self._get_season_url(self.base_url, self.headers)
+
+    def _get_season_url(self, base_url: str, headers: dict[str, str]) -> str | None:
+        r = self._get_request(base_url, headers)
+        if r is None:
+            return None
+        try:
+            html = HTMLParser(r.text)
+            href = next(
+                (
+                    link.attributes["href"]
+                    for link in html.css("div.header-nav-folder-item > a")
+                    if "season" in link.text().lower()
+                ),
+                "",
+            )
+            return f"{base_url}{href}"
+        except Exception as exc:
+            logging.error(f"Unable to fetch season url: {exc}.")
+            return None
+
+    def _build_event_links(self, html: HTMLParser, base_url: str) -> list[str] | None:
+        if links := html.css("a.sqs-block-image-link"):
+            hrefs = [
+                link.css_first("a").attrs.get("href")
+                for link in links
+                if link.css_first("a").attrs.get("href") is not None
+            ]
+            return [f"{base_url}{href}" for href in hrefs if "https" not in href]
+        else:
+            logging.error("No links for individual events found.")
+            return None
+
+    def get_event_urls(self, season_url: str) -> list[str] | None:
+        r = self._get_request(season_url, self.headers)
+        if r is None:
+            return None
+        html = HTMLParser(r.text)
+        return self._build_event_links(html, self.base_url)
+
+    def get_event_data(
+        self, event_urls: list[str], title_tag: str, date_tag: str, image_tag: str
+    ) -> list[dict]:
+        result = []
+        with httpx.Client(headers=self.headers) as client:
+            for url in event_urls:
+                try:
+                    r = client.get(url, headers=self.headers)
+                    html = HTMLParser(r.text)
+                    gig = PhoenixGig(
+                        date=html.css_first(date_tag).text(),
+                        title=html.css_first(title_tag).text(),
+                        url=url,
+                        image=get_image(html, image_tag),
+                    )
+                    result.append(gig.model_dump())
+                except Exception as exc:
+                    logging.error(f"Unable to extract data from URL '{url}': {exc}.")
+        return result
 
 
-@timer
 @logger(filepath=save_path("data", "app.log"))
 def phoenix():
     logging.warning(f"Running {os.path.basename(__file__)}")
 
-    IMAGE_TAG = "meta[property='og:image']"
-    TITLE_TAG = "div.sqs-html-content > h3"
-    DATE_TAG = "div.sqs-html-content > h4"
-    headers = custom_headers
-    base_url = "https://phoenixcentralpark.com.au"
+    # Tag variables
+    image_tag = "meta[property='og:image']"
+    title_tag = "div.sqs-html-content > h3"
+    date_tag = "div.sqs-html-content > h4"
 
-    url = get_season_url(base_url, headers)
-    if url is None:
+    # The cool stuff happens here :)
+    scraper = PhoenixScraper()
+    season_url = scraper.current_season_url
+    if season_url is None:
         sys.exit(1)
 
-    urls = get_event_urls(url, headers)
-    if urls is None:
-        logging.error(f"No events found at {url}.")
+    event_urls = scraper.get_event_urls(season_url)
+    if event_urls is None:
         sys.exit(1)
 
-    data = get_data(urls, headers, TITLE_TAG, DATE_TAG, IMAGE_TAG)
-    logging.warning(f"Found {len(data)} events.")
-
-    export_json(data, filepath=save_path("data", "phoenix.json"))
+    data = scraper.get_event_data(event_urls, title_tag, date_tag, image_tag)
+    scraper.export_json(data, filepath=save_path("data", "phoenix.json"))
 
 
 if __name__ == "__main__":
