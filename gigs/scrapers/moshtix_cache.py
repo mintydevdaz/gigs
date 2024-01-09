f31c7bf1f5daeed060e046ab94d9de9d8ea2a76e
mintydevdaz <mintydevdaz@proton.me>
2024-01-09 23:23:52 +1100

    updated with class objects

--- gigs/scrapers/moshtix_cache.py
+++ gigs/scrapers/moshtix_cache.py
@@ -6,56 +6,57 @@ import chompjs
 import httpx
 from selectolax.parser import HTMLParser, Node
 
-from gigs.utils import (
-    export_json,
-    get_request,
-    custom_headers,
-    logger,
-    save_path,
-    timer,
-)
+from gigs.utils import Gig, WebScraper, custom_headers, export_json, logger, save_path, timer
 
 
-def get_response_and_parse_html(url: str, headers: dict[str, str]) -> HTMLParser | None:
-    response = get_request(url, headers)
-    return None if response is None else HTMLParser(response.text)
+class MoshtixScraper(WebScraper):
+    def __init__(self) -> None:
+        super().__init__()
+        self.base_url = (
+            "https://www.moshtix.com.au/v2/search?query=&CategoryList=2&refine="
+        )
+        self._headers = custom_headers
+        self._page_tag = "section.moduleseparator"
+        self._json_tag = "script[type='application/ld+json']"
+        self._event_tag = "div.searchresult.clearfix"
+        self.end_page = self._get_end_page()
+        self.event_nodes = self._get_event_nodes()
 
+    def _extract_page_number(self, response: httpx.Response) -> int | None:
+        html = HTMLParser(response.text)
+        try:
+            pagination_text = html.css_first(self._page_tag).text(strip=True).split(" ")
+            page_num = int(pagination_text[-1]) + 1
+            logging.warning(f"End page is {page_num}.")
+            return page_num
+        except Exception as exc:
+            logging.error(f"Unable to extract last page: {exc}.")
+            return None
 
-def get_page_number(html: HTMLParser, tag: str) -> int | None:
-    try:
-        pagination_text = html.css_first(tag).text(strip=True).split(" ")
-        page_num = int(pagination_text[-1]) + 1
-        logging.warning(f"End page is {page_num}.")
-        return page_num
-    except Exception as exc:
-        logging.error(f"Unable to extract last page: {exc}.")
-        return None
+    def _get_end_page(self, start_page: int = 1) -> int | None:
+        url = f"{self.base_url}{start_page}"
+        r = self._get_request(url, self._headers)
+        return None if r is None else self._extract_page_number(r)
 
+    def _get_event_nodes(self, start_page: int = 1) -> list[Node] | None:
+        end_page = self.end_page
+        if end_page is None:
+            return None
 
-def end_page(
-    base_url: str, headers: dict[str, str], tag: str, start_page: int = 1
-) -> int | None:
-    url = f"{base_url}{start_page}"
-    html = get_response_and_parse_html(url, headers)
-    return None if html is None else get_page_number(html, tag)
-
-
-def get_event_nodes(
-    base_url: str, headers: dict[str, str], json_tag: str, start_page: int = 1, end_page: int = 2
-) -> list[Node]:
-    event_nodes = []
-    with httpx.Client(headers=headers) as client:
-        for page in range(start_page, end_page):
-            url = f"{base_url}{page}"
-            logging.warning(url)
-            try:
-                response = client.get(url)
-                tree = HTMLParser(response.text)
-                nodes = tree.css(json_tag)
-                event_nodes.extend(nodes)
-            except Exception as exc:
-                logging.error(f"Unable to fetch JSON nodes at URL '{url}': {exc}.")
-    return event_nodes
+        # Extract nodes from each page
+        result = []
+        with httpx.Client(headers=self._headers) as client:
+            for page in range(start_page, end_page):
+                url = f"{self.base_url}{page}"
+                logging.warning(url)
+                try:
+                    r = client.get(url)
+                    html = HTMLParser(r.text)
+                    nodes = html.css(self._json_tag)
+                    result.extend(nodes)
+                except Exception as exc:
+                    logging.error(f"Unable to fetch JSON nodes at URL '{url}': {exc}.")
+        return result
 
 
 def extract_event_data(event_nodes: list[Node]) -> list[dict]:
@@ -70,22 +71,12 @@ def extract_event_data(event_nodes: list[Node]) -> list[dict]:
 @logger(filepath=save_path("data", "app.log"))
 def moshtix_cache():
     logging.warning(f"Running {os.path.basename(__file__)}")
-
-    PAGE_TAG = "section.moduleseparator"
-    JSON_TAG = "script[type='application/ld+json']"
-    EVENT_TAG = "div.searchresult.clearfix"
-    base_url = "https://www.moshtix.com.au/v2/search?query=&CategoryList=2&refine="
-    destination_file = "moshtix_cache.json"
-    headers = custom_headers
-
-    page_num = end_page(base_url, headers, PAGE_TAG)
-    if page_num is None:
-        logging.error("Error fetching last page number.")
+    event_nodes = MoshtixScraper().event_nodes
+    if event_nodes is None:
         sys.exit(1)
-
-    event_nodes = get_event_nodes(base_url, headers, JSON_TAG, end_page=page_num)
     data = extract_event_data(event_nodes)
-    logging.warning(f"Found {len(data)} events.")
+
+    destination_file = "moshtix_cache.json"
     export_json(data, filepath=save_path("cache", destination_file))
 
 
